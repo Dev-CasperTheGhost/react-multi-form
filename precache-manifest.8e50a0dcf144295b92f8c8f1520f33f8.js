494aa4d4bfb286a612f9109e5c25533079fd18a9
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-06-19 16:22:18 +0200

    Updates

---
+++ precache-manifest.8e50a0dcf144295b92f8c8f1520f33f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0520dce0e99d52565bebd873fac0fec2",
+    "url": "/react-multi-form/index.html"
+  },
+  {
+    "revision": "06a963a942d58b494b40",
+    "url": "/react-multi-form/static/css/main.2723af11.chunk.css"
+  },
+  {
+    "revision": "43a5fb1dd970f285402b",
+    "url": "/react-multi-form/static/js/2.84402d49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-multi-form/static/js/2.84402d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a963a942d58b494b40",
+    "url": "/react-multi-form/static/js/main.5568e4eb.chunk.js"
+  },
+  {
+    "revision": "268c2fe1d01c3a792bfe",
+    "url": "/react-multi-form/static/js/runtime-main.99499a6e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c74d805645e12fd000b50941acab2d36.js"
+  "/react-multi-form/precache-manifest.8e50a0dcf144295b92f8c8f1520f33f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-multi-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
